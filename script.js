286263c4df7b6040d7694597f9b6e861f0ef441b
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-09-30 17:36:31 -0400

    added pseudo code to js file, and initial html code

---
+++ script.js
@@ -0,0 +1,10 @@
+/*a button with an eventLister "Click".
+object called word with keys and values. 
+10 words with 5-8 letters
+whenever user enters the letter-- key event --if that letter is present in the word,
+change status to visible. 
+have a setInterval for the timer 30 seconds and clear interval. a timerReset
+win count and loss count
+return status report total wins and total losses
+localStorage for wins and losses
+timer reset on start button "click" */
